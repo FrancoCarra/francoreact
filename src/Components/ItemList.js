8ba5bc059289676acf3a3069da9cc99001f6c35e
FrancoCarra <104314979+FrancoCarra@users.noreply.github.com>
2022-09-07 14:00:29 +0200

    AVANCE

---
+++ src/Components/ItemList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+
+
+
+ export const ItemList = () => {
+return (
+   <>
+    <div>
+
+    </div>
+  </>
+)
+
+}
+export default ItemList;
