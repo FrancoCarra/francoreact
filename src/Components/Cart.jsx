ef91d014d03526ab522950a06bbfc795ad3f9d1a
FrancoCarra <104314979+FrancoCarra@users.noreply.github.com>
2022-10-04 00:56:36 +0200

    ORDEN DE COMPRA

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -2,10 +2,31 @@ import React from "react";
 import { useCartContext } from './CartContext'
 import { Link } from 'react-router-dom'
 import ItemCart from "./ItemCart";
+import { getFirestone, addDoc, collection } from 'firebase/firestone'
 
 const Cart = () => {
 
     const { cart, totalPrice } = useCartContext();
+
+    const order = {
+        buyer: {
+            name: 'Pablo',
+            email: 'pabloperez@gmail.com',
+            phone: '123123',
+            addres: 'Via Marghetina'
+
+        },
+        items: cart.map(product => ({ id: product.id, title: product.title, price: product.price, quantity: product.quantity })),
+        total: totalPrice(),
+    }
+
+    const handleClick = () => {
+        const db = getFirestone();
+        const ordersCollection = collection(db, 'orders');
+        addDoc(ordersCollection, order)
+        .then(({ id }) => console.log)
+
+    }
 
     if(cart.lenght === 0){
     return (
@@ -23,6 +44,7 @@ return (
     <p>
         total: {totalPrice()}
     </p>
+    <button onClick={handleClick}>Emitir Compra</button>
     </>
 )
 }
